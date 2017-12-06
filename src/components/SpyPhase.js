6174b8bd223ad6184c34d61bee97fe94e12b6228
Jake Budge <jakebudge@Jakes-iMac.local>
2017-12-06 16:37:47 -0700

    Fixed audio issue

--- src/components/SpyPhase.js
+++ src/components/SpyPhase.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/spy-phase.css';
+import basicNarration from '../audio/narration.mp3';
 
 const SpyPhase = (props) => {
     return (
         <div className="SpyPhaseContainer">
             <div className="SpyPhaseContainer__text">Mission Leader: Narrate Spy Phase</div>
+            <audio src={basicNarration} controls></audio>
             <button className="SpyPhaseContainer__button" onClick={ props.gameBoard }>Start Game</button>
         </div>
     );
