fd9fd56410e77a1b44292dc5a4b488e57b590ba7
Jake Budge <jakebudge@Jakes-iMac.local>
2017-12-06 16:30:58 -0700

    Removed problematic audio

--- src/components/SpyPhase.js
+++ src/components/SpyPhase.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../styles/spy-phase.css';
-import narration2 from '../audio/resistance-narration2.mp3';
 
 const SpyPhase = (props) => {
     return (
         <div className="SpyPhaseContainer">
             <div className="SpyPhaseContainer__text">Mission Leader: Narrate Spy Phase</div>
-            <audio controls src={ narration2 }>
-            </audio>
             <button className="SpyPhaseContainer__button" onClick={ props.gameBoard }>Start Game</button>
         </div>
     );
